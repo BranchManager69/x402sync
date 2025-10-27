0c17f2fd8b207cd845fef4e42383a362b2b29da9
shafu <shafu0x@gmail.com>
2025-10-27 11:34:59 -0400

    migration comment

--- prisma/migrations/20251027151542_manual_add_chain_sender_recipient_indices/migration.sql
+++ prisma/migrations/20251027151542_manual_add_chain_sender_recipient_indices/migration.sql
@@ -3,3 +3,15 @@
 -- - TransferEvent_chain_block_timestamp_facilitator_id_idx
 -- - TransferEvent_sender_idx
 -- - TransferEvent_recipient_idx
+
+-- Index 1: Triple composite (chain + timestamp + facilitator)
+-- CREATE INDEX CONCURRENTLY "TransferEvent_chain_block_timestamp_facilitator_id_idx" 
+-- ON "TransferEvent"("chain", "block_timestamp", "facilitator_id");
+
+-- Index 2: Sender
+-- CREATE INDEX CONCURRENTLY "TransferEvent_sender_idx" 
+-- ON "TransferEvent"("sender");
+
+-- Index 3: Recipient  
+-- CREATE INDEX CONCURRENTLY "TransferEvent_recipient_idx"
+-- ON "TransferEvent"("recipient");
